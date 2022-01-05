122e7c90565ef6964b881a9d230757061d635208
andbandu <andbanduln@gmail.com>
2022-01-05 08:24:54 +0530

    initial

---
+++ contracts/Wikara.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract Wikara{
+    uint256 public totalSupply;
+
+    constructor(){
+        totalSupply = 10000;
+    }
+}
---
+++ migrations/2_wikara_migration.js
@@ -0,0 +1,5 @@
+const Wikara = artifacts.require("Wikara");
+
+module.exports = function (deployer) {
+    deployer.deploy(Wikara);
+};
